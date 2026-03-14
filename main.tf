cb41ffdb1fb0f0a0d8d462b9553b519e3a1b6854
rabi-m <capital861@gmail.com>
2026-03-14 15:52:36 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_instance" "devops_server" {
+ ami           = "ami-123456"
+ instance_type = "t2.micro"
+
+ tags = {
+   Name = "DevOpsServer"
+ }
+}
